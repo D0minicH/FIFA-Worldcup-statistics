7d4124a116a430e2c745f8e0ba24935bad4c1ed2
Dominic Hein <dominic.hein@students.fhnw.ch>
2019-05-09 17:20:12 +0200

    TeamInfos load dynamically from dropdown menu (Left side)

--- src/tournamenttree.js
+++ src/tournamenttree.js
@@ -1,40 +1,51 @@
+
 // set the dimensions and margins of the diagram
-var margin = { top: 40, right: 90, bottom: 50, left: 300 },
+var margin = { top: 40, right: 0, bottom: 50, left: 240 },
   width = 1100 - margin.left - margin.right,
   height = 650 - margin.top - margin.bottom,
   separationConstant = 1;
 
 var treeData = {
   a: "France",
+  flaga:"https://api.fifa.com/api/v1/picture/flags-fwc2018-5/fra",
   b: "Croatia",
+  flagb:"https://api.fifa.com/api/v1/picture/flags-fwc2018-5/cro",
   ascore: 4,
   bscore: 2,
   win: true,
   children: [
     {
       a: "France",
+      flaga:"https://api.fifa.com/api/v1/picture/flags-fwc2018-5/fra",
       b: "Belgium",
+      flagb:"https://api.fifa.com/api/v1/picture/flags-fwc2018-5/bel",
       ascore: 1,
       bscore: 0,
       win: true,
       children: [
         {
           a: "Uruguay",
+          flaga:"https://api.fifa.com/api/v1/picture/flags-fwc2018-5/uru",
           b: "France",
+          flagb:"https://api.fifa.com/api/v1/picture/flags-fwc2018-5/fra",
           ascore: 0,
           bscore: 2,
           win: true,
           children: [
             {
               a: "France",
+              flaga:"https://api.fifa.com/api/v1/picture/flags-fwc2018-5/fra",
               b: "Argentina",
+              flagb:"https://api.fifa.com/api/v1/picture/flags-fwc2018-5/arg",
               ascore: 4,
               bscore: 2,
               win: true,
             },
             {
               a: "Uruguay",
+              flaga:"https://api.fifa.com/api/v1/picture/flags-fwc2018-5/uru",
               b: "Portugal",
+              flagb:"https://api.fifa.com/api/v1/picture/flags-fwc2018-5/por",
               ascore: 2,
               bscore: 1,
             }
@@ -42,21 +53,27 @@ var treeData = {
         },
         {
           a: "Brasil",
+          flaga:"https://api.fifa.com/api/v1/picture/flags-fwc2018-5/bra",
           b: "Belgium",
+          flagb:"https://api.fifa.com/api/v1/picture/flags-fwc2018-5/bel",
           ascore: 1,
           bscore: 2,
           win: false,
           children: [
             {
               a: "Brasil",
+              flaga:"https://api.fifa.com/api/v1/picture/flags-fwc2018-5/bra",
               b: "Mexico",
+              flagb:"https://api.fifa.com/api/v1/picture/flags-fwc2018-5/mex",
               ascore: 2,
               bscore: 0,
               win: false
             },
             {
               a: "Belgium",
+              flaga:"https://api.fifa.com/api/v1/picture/flags-fwc2018-5/bel",
               b: "Japan",
+              flagb:"https://api.fifa.com/api/v1/picture/flags-fwc2018-5/jpn",
               ascore: 3,
               bscore: 2,
               win: false
@@ -67,28 +84,36 @@ var treeData = {
     },
     {
       a: "Croatia",
+      flaga:"https://api.fifa.com/api/v1/picture/flags-fwc2018-5/cro",
       b: "England",
+      flagb:"https://api.fifa.com/api/v1/picture/flags-fwc2018-5/eng",
       ascore: 2,
       bscore: 1,
       win: true,
       children: [
         {
           a: "Russia",
+          flaga:"https://api.fifa.com/api/v1/picture/flags-fwc2018-5/rus",
           b: "Croatia",
+          flagb:"https://api.fifa.com/api/v1/picture/flags-fwc2018-5/cro",
           ascore: 2,
           bscore: 2,
           win: true,
           children: [
             {
               a: "Spain",
+              flaga:"https://api.fifa.com/api/v1/picture/flags-fwc2018-5/esp",
               b: "Russia",
+              flagb:"https://api.fifa.com/api/v1/picture/flags-fwc2018-5/rus",
               ascore: 1,
               bscore: 1,
               win: false
             },
             {
               a: "Croatia",
+              flaga:"https://api.fifa.com/api/v1/picture/flags-fwc2018-5/cro",
               b: "Denmark",
+              flagb:"https://api.fifa.com/api/v1/picture/flags-fwc2018-5/den",
               ascore: 1,
               bscore: 1,
               win: true
@@ -97,21 +122,27 @@ var treeData = {
         },
         {
           a: "Sweden",
+          flaga:"https://api.fifa.com/api/v1/picture/flags-fwc2018-5/swe",
           b: "England",
+          flagb:"https://api.fifa.com/api/v1/picture/flags-fwc2018-5/eng",
           ascore: 0,
           bscore: 2,
           win: false,
           children: [
             {
               a: "Sweden",
+              flaga:"https://api.fifa.com/api/v1/picture/flags-fwc2018-5/swe",
               b: "Switzerland",
+              flagb:"https://api.fifa.com/api/v1/picture/flags-fwc2018-5/sui",
               ascore: 1,
               bscore: 0,
               win: false
             },
             {
               a: "Columbia",
+              flaga:"https://api.fifa.com/api/v1/picture/flags-fwc2018-5/col",
               b: "England",
+              flagb:"https://api.fifa.com/api/v1/picture/flags-fwc2018-5/eng",
               ascore: 1,
               bscore: 1,
               win: false
@@ -166,11 +197,13 @@ var link = g
 function gameTemplate(d) {
   return (
     "" + "<div class='row" + (d.data.ascore > d.data.bscore ? ' winner' : '') + "'>" +
+    "<img class='cell flag'src=" + (d.data.flaga) +">" +
     "<span class='cell name'>" + d.data.a + "</span>" + 
     "<span class='cell score'>" + (d.data.ascore >= 0 ? d.data.ascore : '') + "</span>" +  
   "</div>" + 
   
   "<div class='row" + (d.data.bscore > d.data.ascore ? ' winner' : '') + "'>" +
+    "<img class='cell flag' src=" + (d.data.flagb) + ">" +  
     "<span class='cell name'>" + (d.data.b || '') + "</span>" + 
     "<span class='cell score'>" + (d.data.bscore >= 0 ? d.data.bscore : '') + "</span>" +  
   "</div>"
@@ -197,7 +230,9 @@ var labels = d3
 var thrdData = {
   data: {
     a: "Belgium",
+    flaga:"https://api.fifa.com/api/v1/picture/flags-fwc2018-5/bel",
     b: "England",
+    flagb:"https://api.fifa.com/api/v1/picture/flags-fwc2018-5/eng",
     ascore: 2,
     bscore: 0
   }
@@ -208,7 +243,7 @@ var thrd = d3
   .append("div")
   .classed("thirdplace", true)
   .style("position", "absolute")
-  .style("left", width -35+ "px")
+  .style("left", width -145+ "px")
   .style("top", height / 2 + margin.top - 48 + "px");
 
 thrd
